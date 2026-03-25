55162498d617f128b7ca495fa4f00fe8710234fa
SrutiGupta <mg6156710@gmail.com>
2026-03-25 16:50:24 +0530

    Authentication

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,12 @@ Route::get('/', function () {
 Route::get('/form', function () {
     return view('form');
 });
+
+use App\Http\Controllers\AuthController;
+
+Route::get('/signup',  [AuthController::class, 'signupPage']);
+Route::post('/signup', [AuthController::class, 'signupSubmit']);
+Route::get('/signin',  [AuthController::class, 'signinPage']);
+Route::post('/signin', [AuthController::class, 'signinSubmit']);
+Route::get('/logout',  [AuthController::class, 'logout']);
+Route::post('/submit', [FormController::class, 'submit']);
